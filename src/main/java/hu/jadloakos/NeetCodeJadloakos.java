129751de09b03be880b694a625e6a1c6696424de
Jadlószki Ákos <akos.jadloszki@ingroom.hu>
2023-02-09 08:56:13 +0100

    [main] Add basic project structure.

---
+++ src/main/java/hu/jadloakos/NeetCodeJadloakos.java
@@ -0,0 +1,9 @@
+package hu.jadloakos;
+
+/** Main class */
+public class NeetCodeJadloakos {
+
+  public static void main(String[] args) {
+    System.out.println("This project contains solutions for neet code problems!");
+  }
+}
---
+++ src/test/java/hu/jadloakos/NeetCodeJadloakosTest.java
@@ -0,0 +1,14 @@
+package hu.jadloakos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/** Unit test for NettCodeJadloakos App. */
+public class NeetCodeJadloakosTest {
+
+  @Test
+  public void testNeetCodeJadloakos() {
+    assertTrue(true);
+  }
+}
